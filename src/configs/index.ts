7a2cf66d46cdc4ac020d9b8dda37c760ca4b6c67
nybble <nybble@yandex-team.ru>
2021-10-21 16:02:04 +0300

    add resolution param

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,5 +3,5 @@ export type Config = {
 }
 
 export default ({
-    query: 'Assignee: me() and Status: Closed and Resolved: >2021-04-01'
+    query: 'Assignee: me() and Resolution: fixed and Status: Closed and Resolved: >2021-04-01'
 }) as Config;
